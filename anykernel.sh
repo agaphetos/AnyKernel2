419466906d299a0d89136c157f5e27462f4341f9
Levin Calado <levincalado@gmail.com>
2015-11-15 10:01:07 +0800

    anykernel.sh: support for sprout
    Signed-off-by: Levin Calado <levincalado@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -3,19 +3,19 @@
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=DirtyV by bsmitty83 @ xda-developers
+kernel.string=#.kernel_string.#
 do.devicecheck=1
 do.initd=1
 do.modules=0
 do.cleanup=1
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=sprout
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/mtk-msdc.0/by-name/boot;
 
 ## end setup
 
